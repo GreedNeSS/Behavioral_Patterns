a50af1f5ba00d320fdcfe7652c1901336e3ec272
GreedNeSS <ruslan199126@gmail.com>
2022-09-15 19:37:24 +0300

    Added implementations and model(context).

---
+++ 1-Strategy/Implementations/PetrolEngine.cs
@@ -0,0 +1,17 @@
+﻿using _1_Strategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Strategy.Implementations
+{
+    public class PetrolEngine : IEngine
+    {
+        public void Move()
+        {
+            Console.WriteLine("Двигаемся с помощью бензина.");
+        }
+    }
+}
---
+++ 1-Strategy/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using _1_Strategy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Strategy.Models
+{
+    public class Car
+    {
+        public int Passengers { get; set; }
+        public string Model { get; set; }
+        public IEngine Engine{ get; set; }
+
+        public Car(int passengers, string model, IEngine engine)
+        {
+            Passengers = passengers;
+            Model = model;
+            Engine = engine;
+        }
+
+        public void Move()
+        {
+            Engine.Move();
+        }
+    }
+}
