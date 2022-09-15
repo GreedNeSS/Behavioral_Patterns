0da505f79fa6bb17808b2153ad27d3b074bc4158
GreedNeSS <ruslan199126@gmail.com>
2022-09-15 19:41:37 +0300

    Adde new example.

--- 1-Strategy/Program.cs
+++ 1-Strategy/Program.cs
@@ -2,3 +2,10 @@
 using _1_Strategy.Models;
 
 Car car = new Car(5, "Lada", new PetrolEngine());
+car.Move();
+car.SwapEngine(new GasEngine());
+car.Move();
+car.SwapEngine(new HybridEngine());
+car.Move();
+
+Console.ReadLine();
