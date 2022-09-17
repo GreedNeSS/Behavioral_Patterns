dc8c3bf219ec2685a0bfe22948e43cf03d1f7be8
GreedNeSS <ruslan199126@gmail.com>
2022-09-17 14:11:16 +0300

    Added interfaces.

---
+++ 2-Observer/Interfaces/IObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Observer.Interfaces
+{
+    public interface IObservable
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+}
