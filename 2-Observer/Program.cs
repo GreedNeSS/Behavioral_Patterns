43b0e8817a6fe5b570d46e07881a5c4bc5d59a76
GreedNeSS <ruslan199126@gmail.com>
2022-09-17 14:35:45 +0300

    Added new example.

--- 2-Observer/Program.cs
+++ 2-Observer/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2_Observer.Implementations;
+
+Stock stock = new Stock();
+Broker broker = new Broker("Аристарх Монетович", stock);
+Bank bank = new Bank("Сбер", stock);
+
+stock.Market();
+stock.Market();
+broker.StopTrade();
+stock.Market();
+stock.Market();
+
+Console.ReadLine();
