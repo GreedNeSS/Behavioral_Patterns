953faae88ff41c1003f8c9591be5af42b480c39b
GreedNeSS <ruslan199126@gmail.com>
2022-09-17 14:30:13 +0300

    Added observer implementations.

---
+++ 2-Observer/Implementations/Bank.cs
@@ -0,0 +1,31 @@
+﻿using _2_Observer.Interfaces;
+using _2_Observer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Observer.Implementations
+{
+    public class Bank : IObserver
+    {
+        public string Name { get; set; }
+
+        public Bank(string name, IObservable observable)
+        {
+            Name = name;
+            observable.RegisterObserver(this);
+        }
+
+        public void Update(object obj)
+        {
+            StockInfo stockInfo = (StockInfo) obj;
+
+            if (stockInfo.Euro > 60)
+                Console.WriteLine("Банк {0} продает евро;  Курс евро: {1}", this.Name, stockInfo.Euro);
+            else
+                Console.WriteLine("Банк {0} покупает евро;  Курс евро: {1}", this.Name, stockInfo.Euro);
+        }
+    }
+}
---
+++ 2-Observer/Implementations/Broker.cs
@@ -0,0 +1,39 @@
+﻿using _2_Observer.Interfaces;
+using _2_Observer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Observer.Implementations
+{
+    public class Broker : IObserver
+    {
+        public string Name { get; set; }
+        IObservable? stock;
+
+        public Broker(string name, IObservable stock)
+        {
+            Name = name;
+            this.stock = stock;
+            stock.RegisterObserver(this);
+        }
+
+        public void Update(object obj)
+        {
+            StockInfo stockInfo = (StockInfo)obj;
+
+            if (stockInfo.USD > 55)
+                Console.WriteLine("Брокер {0} продает доллары;  Курс доллара: {1}", this.Name, stockInfo.USD);
+            else
+                Console.WriteLine("Брокер {0} покупает доллары;  Курс доллара: {1}", this.Name, stockInfo.USD);
+        }
+
+        public void StopTrade()
+        {
+            stock?.RemoveObserver(this);
+            stock = null;
+        }
+    }
+}
