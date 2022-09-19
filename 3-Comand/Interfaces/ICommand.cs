e89f1d2239620193b7fab202ec060135f0b6dd83
GreedNeSS <ruslan199126@gmail.com>
2022-09-19 17:42:07 +0300

    Added command interface.

---
+++ 3-Comand/Interfaces/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Comand.Interfaces
+{
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
