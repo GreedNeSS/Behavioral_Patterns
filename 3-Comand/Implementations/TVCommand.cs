2236b5c1d36b7077977802b27076e73ab1fa1e78
GreedNeSS <ruslan199126@gmail.com>
2022-09-19 17:49:04 +0300

    Added command implementation.

---
+++ 3-Comand/Implementations/TVCommand.cs
@@ -0,0 +1,30 @@
+﻿using _3_Comand.Interfaces;
+using _3_Comand.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Comand.Implementations
+{
+    public class TVCommand : ICommand
+    {
+        TV _TV;
+
+        public TVCommand(TV tV)
+        {
+            _TV = tV;
+        }
+
+        public void Execute()
+        {
+            _TV.OnTV();
+        }
+
+        public void Undo()
+        {
+            _TV.OffTV();
+        }
+    }
+}
