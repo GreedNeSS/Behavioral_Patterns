b5f86f01fcf8e941c089550cae9ba7f239d0799c
GreedNeSS <ruslan199126@gmail.com>
2022-09-19 17:44:49 +0300

    Added TV model.

---
+++ 3-Comand/Models/TV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Comand.Models
+{
+    public class TV
+    {
+        public void OnTV()
+        {
+            Console.WriteLine("Включение телевизора");
+        }
+        public void OffTV()
+        {
+            Console.WriteLine("Выключение телевизора");
+        }
+    }
+}
