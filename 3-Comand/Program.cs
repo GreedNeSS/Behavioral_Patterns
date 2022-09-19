e1dbdf66c90a2831281f873a910f3af3a5cb5a04
GreedNeSS <ruslan199126@gmail.com>
2022-09-19 18:04:05 +0300

    Added new example.

--- 3-Comand/Program.cs
+++ 3-Comand/Program.cs
@@ -1,5 +1,16 @@
 ﻿using _3_Comand.Implementations;
 using _3_Comand.Models;
 
+// Receiver
 TV someTV = new TV();
+
+// Command
 TVCommand command = new TVCommand(someTV);
+
+// Invoker
+RemoteController remote = new RemoteController();
+remote.SetCommand(command);
+remote.PressButton();
+remote.PressUndo();
+
+Console.ReadLine();
