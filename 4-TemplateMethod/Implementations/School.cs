7171ba819eb37f4b48cb31a1c8b7af40efda11ae
GreedNeSS <ruslan199126@gmail.com>
2022-09-21 22:24:49 +0300

    Added school.

---
+++ 4-TemplateMethod/Implementations/School.cs
@@ -0,0 +1,27 @@
+﻿using _4_TemplateMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_TemplateMethod.Implementations
+{
+    public class School : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Идём в первый класс");
+        }
+
+        public override void GetDocument()
+        {
+            Console.WriteLine("Посещаем уроки, делаем домашние задания");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Получаем аттестат о среднем образовании");
+        }
+    }
+}
