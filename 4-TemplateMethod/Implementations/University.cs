057b6a3ad99cefcca03d9adda111ac726c684248
GreedNeSS <ruslan199126@gmail.com>
2022-09-21 22:26:13 +0300

    Added University class.

---
+++ 4-TemplateMethod/Implementations/University.cs
@@ -0,0 +1,33 @@
+﻿using _4_TemplateMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_TemplateMethod.Implementations
+{
+    public class University : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Сдаем вступительные экзамены и поступаем в ВУЗ");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Посещаем лекции");
+            Console.WriteLine("Проходим практику");
+        }
+
+        public override void PassExams()
+        {
+            Console.WriteLine("Сдаем экзамен по специальности");
+        }
+
+        public override void GetDocument()
+        {
+            Console.WriteLine("Получаем диплом о высшем образовании");
+        }
+    }
+}
