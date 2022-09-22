6609fc74fa61ff58ab9b76a89329d09e3013799d
GreedNeSS <ruslan199126@gmail.com>
2022-09-22 18:42:10 +0300

    Added IBookIterator.

---
+++ 5-Iterator/Interfaces/IBookIterator.cs
@@ -0,0 +1,15 @@
+﻿using _5_Iterator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Iterator.Interfaces
+{
+    public interface IBookIterator
+    {
+        bool HAsNext();
+        Book Next();
+    }
+}
