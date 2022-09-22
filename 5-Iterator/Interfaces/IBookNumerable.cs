7ae349d7961fdeb6e46b5595f4a28bbd754369d5
GreedNeSS <ruslan199126@gmail.com>
2022-09-22 18:44:41 +0300

    Added IBookNumerable.

---
+++ 5-Iterator/Interfaces/IBookNumerable.cs
@@ -0,0 +1,16 @@
+﻿using _5_Iterator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Iterator.Interfaces
+{
+    public interface IBookNumerable
+    {
+        IBookIterator CreateNumerable();
+        int Count { get; }
+        Book this[int index] { get; }
+    }
+}
