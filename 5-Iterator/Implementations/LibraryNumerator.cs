75567958a7a0709845156b7c3482d2d5c016ef71
GreedNeSS <ruslan199126@gmail.com>
2022-09-22 18:54:46 +0300

    Added IBookIterator implementation.

---
+++ 5-Iterator/Implementations/LibraryNumerator.cs
@@ -0,0 +1,31 @@
+﻿using _5_Iterator.Interfaces;
+using _5_Iterator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Iterator.Implementations
+{
+    public class LibraryNumerator : IBookIterator
+    {
+        private IBookNumerable aggregate;
+        private int index = 0;
+
+        public LibraryNumerator(IBookNumerable aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+
+        public bool HAsNext()
+        {
+            return aggregate.Count > index;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
