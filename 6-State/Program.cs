f053f380dbb79e19395ad06c9e83b6c95dd73f4c
GreedNeSS <ruslan199126@gmail.com>
2022-09-26 21:41:16 +0300

    Added new example.

--- 6-State/Program.cs
+++ 6-State/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _6_State.Contexts;
+using _6_State.StateImplementations;
+
+Water water = new Water(new LiquidWaterState());
+water.Heat();
+water.Heat();
+water.Frost();
+water.Frost();
+water.Frost();
+
+Console.ReadLine();
