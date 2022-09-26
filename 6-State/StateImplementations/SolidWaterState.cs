aefc94de0d08810a3ad7de3dc287015c70b3a101
GreedNeSS <ruslan199126@gmail.com>
2022-09-26 21:34:56 +0300

    Added SolidWaterState.

---
+++ 6-State/StateImplementations/SolidWaterState.cs
@@ -0,0 +1,24 @@
+﻿using _6_State.Contexts;
+using _6_State.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_State.StateImplementations
+{
+    public class SolidWaterState : IWaterState
+    {
+        public void Frost(Water water)
+        {
+            Console.WriteLine("Продолжаем морозить лёд");
+        }
+
+        public void Heat(Water water)
+        {
+            Console.WriteLine("Превращаем лед в воду");
+            water.State = new LiquidWaterState();
+        }
+    }
+}
