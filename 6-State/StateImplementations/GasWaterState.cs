fcf7d5d84b590eab01eeaaad98b1f9f616406516
GreedNeSS <ruslan199126@gmail.com>
2022-09-26 21:39:31 +0300

    Added GasWaterState.

---
+++ 6-State/StateImplementations/GasWaterState.cs
@@ -0,0 +1,24 @@
+﻿using _6_State.Contexts;
+using _6_State.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_State.StateImplementations
+{
+    public class GasWaterState : IWaterState
+    {
+        public void Frost(Water water)
+        {
+            Console.WriteLine("Превращаем газ в воду");
+            water.State = new LiquidWaterState();
+        }
+
+        public void Heat(Water water)
+        {
+            Console.WriteLine("Продолжаем нагревать водяной пар");
+        }
+    }
+}
