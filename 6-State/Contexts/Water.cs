23de08f511f0024170b66ba058b7ea73a89fbe8f
GreedNeSS <ruslan199126@gmail.com>
2022-09-26 21:30:06 +0300

    Added context.

---
+++ 6-State/Contexts/Water.cs
@@ -0,0 +1,29 @@
+﻿using _6_State.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_State.Contexts
+{
+    public class Water
+    {
+        public IWaterState State{ get; set; }
+
+        public Water(IWaterState state)
+        {
+            State = state;
+        }
+
+        public void Heat()
+        {
+            State.Heat(this);
+        }
+
+        public void Frost()
+        {
+            State.Frost(this);
+        }
+    }
+}
