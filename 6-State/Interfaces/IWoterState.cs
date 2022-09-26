d88fafc39ff072b071801d971250ae0583885a5a
GreedNeSS <ruslan199126@gmail.com>
2022-09-26 21:26:57 +0300

    Added IWaterState interface.

---
+++ 6-State/Interfaces/IWoterState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_State.Interfaces
+{
+    public interface IWaterState
+    {
+        void Heat(Water water);
+        void Frost(Water water);
+    }
+}
