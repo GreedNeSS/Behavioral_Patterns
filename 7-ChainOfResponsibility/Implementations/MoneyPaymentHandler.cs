5c428f2e54cff50d8cf8971160cd7147e97caf41
GreedNeSS <ruslan199126@gmail.com>
2022-10-25 18:37:11 +0300

    Added MoneyPaymentHandler implementation.

---
+++ 7-ChainOfResponsibility/Implementations/MoneyPaymentHandler.cs
@@ -0,0 +1,27 @@
+﻿using _7_ChainOfResponsibility.Interfaces;
+using _7_ChainOfResponsibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ChainOfResponsibility.Implementations
+{
+    public class MoneyPaymentHandler : PaymentHandler
+    {
+        public PaymentHandler? Successor { get; set; }
+
+        public void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer)
+            {
+                Console.WriteLine("Выполняем перевод через системы денежных переводов");
+            }
+            else if (Successor != null)
+            {
+                Successor.Handle(receiver);
+            }
+        }
+    }
+}
