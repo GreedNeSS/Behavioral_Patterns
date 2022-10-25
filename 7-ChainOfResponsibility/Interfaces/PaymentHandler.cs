b9593af0cb6515c1523b3486ecc4820da01a8651
GreedNeSS <ruslan199126@gmail.com>
2022-10-25 18:28:38 +0300

    Added handler interface.

---
+++ 7-ChainOfResponsibility/Interfaces/PaymentHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _7_ChainOfResponsibility.Models;
+
+namespace _7_ChainOfResponsibility.Interfaces
+{
+    public interface PaymentHandler
+    {
+        PaymentHandler Successor { get; set; }
+        void Handle(Receiver receiver);
+    }
+}
