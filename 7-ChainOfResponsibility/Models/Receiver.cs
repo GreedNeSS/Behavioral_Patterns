a87b237fc3c48854b8c8a789de1d85189cd7692a
GreedNeSS <ruslan199126@gmail.com>
2022-10-25 18:23:56 +0300

    Added Receiver.

---
+++ 7-ChainOfResponsibility/Models/Receiver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ChainOfResponsibility.Models
+{
+    public class Receiver
+    {
+        public bool BankTransfer { get; set; }
+        public bool MoneyTransfer { get; set; }
+        public bool PayPalTransfer { get; set; }
+        public int TransferAmount { get; set; }
+
+        public Receiver(bool bankTransfer, bool moneyTransfer, bool payPalTransfer, int transferAmount)
+        {
+            BankTransfer = bankTransfer;
+            MoneyTransfer = moneyTransfer;
+            PayPalTransfer = payPalTransfer;
+            TransferAmount = transferAmount;
+        }
+    }
+}
