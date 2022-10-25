52639157c98b682f780e9bca77b7063bc5bc382a
GreedNeSS <ruslan199126@gmail.com>
2022-10-25 18:38:39 +0300

    Added PayPalPaymentHandler implementation.

---
+++ 7-ChainOfResponsibility/Implementations/PayPalPaymentHandler.cs
@@ -0,0 +1,27 @@
+﻿using _7_ChainOfResponsibility.Interfaces;
+using _7_ChainOfResponsibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ChainOfResponsibility.Implementations
+{
+    public class PayPalPaymentHandler : PaymentHandler
+    {
+        public PaymentHandler? Successor { get; set; }
+
+        public void Handle(Receiver receiver)
+        {
+            if (receiver.PayPalTransfer)
+            {
+                Console.WriteLine("Выполняем перевод через PayPal");
+            }
+            else if (Successor != null)
+            {
+                Successor.Handle(receiver);
+            }
+        }
+    }
+}
