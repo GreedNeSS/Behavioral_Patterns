b1cc4683990a26597b6cbcc3015bb33f38b3c09f
GreedNeSS <ruslan199126@gmail.com>
2022-10-25 18:45:20 +0300

    Added client.

--- 7-ChainOfResponsibility/Program.cs
+++ 7-ChainOfResponsibility/Program.cs
@@ -1,3 +1,15 @@
-﻿using _7_ChainOfResponsibility.Models;
+﻿using _7_ChainOfResponsibility.Implementations;
+using _7_ChainOfResponsibility.Interfaces;
+using _7_ChainOfResponsibility.Models;
 
-Receiver receiver = new Receiver(false)
+Receiver receiver = new Receiver(false, true, true, 100);
+
+PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+PaymentHandler payPalPaymentHandler = new PayPalPaymentHandler();
+PaymentHandler moneyPaymentHandler = new MoneyPaymentHandler();
+bankPaymentHandler.Successor = payPalPaymentHandler;
+payPalPaymentHandler.Successor = moneyPaymentHandler;
+
+bankPaymentHandler.Handle(receiver);
+
+Console.Read();
